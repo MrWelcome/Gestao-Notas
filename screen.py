c4a8d5785a964e37e479aa100d9c696432fa2b09
Matheus Felipe <65515159+mfelipemedeiros@users.noreply.github.com>
2020-10-26 09:13:12 -0300

    Criação da tela de um menu

--- screen.py
+++ screen.py
@@ -1,5 +1,16 @@
 class screen:
     def cabecalho(self):
-        print("--------------------")
-        print("Gestão de Notas")
-        print("--------------------")
+        print("#"*60)
+        print("#"*21,"Gestor de nostas","#"*21)
+        print("#"*60)
+        print("_"*tamanhoTotal)
+        for i in range(1,len_lista ):
+            tamanhoNome = len(nome[i])
+            tamanhoNota = len(nota[i])
+            tamanhoTotal = tamanhoNome + tamanhoNota#pega do tamanho das strings na tabela
+            self = self[i].replace(",","|")#substituir as virgulas por barra
+            self = self[i].insert(0,"|")#adciona uma barra no inicio da linha
+
+        print("_"*tamanhoTotal) 
+        print("#"*14,"NÃO HÁ OUTRAS NOTAS REGISTRADAS","#"*14)
+
